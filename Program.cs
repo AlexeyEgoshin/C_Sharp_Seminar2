8c75480579c884fb32bb99ceb187d94cc54685e5
alex0210 <a0210@yandex.ru>
2023-02-14 21:36:25 +0300

    added seminar3

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// 1. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+int Prompt(string msg)
+{
+    System.Console.Write($"{msg} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = Prompt("Введите 3х значное число");
+if (number >= 100 && number < 1000)
+{
+    int lastDigit = number % 10;
+    System.Console.WriteLine($"последняя цифра числа {number} равна {lastDigit}");
+}
